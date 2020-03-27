337ae123587b01416f8ae3444312f145d2fc2930
Emily Blocker <emily.blocker@leidos.com>
2020-03-27 13:53:37 -0600

    Implemented public methods in maze

--- src/maze/Maze.java
+++ src/maze/Maze.java
@@ -4,6 +4,10 @@ public class Maze {
 	private static Maze Instance = null;
 	private char[][] MazeArray;
 	
+	private Coordinate droneCurrentPosition;
+	private Coordinate goalPosition;
+	private Direction facedDirection;
+	
 	public static Maze getInstance() {
 		if(Instance == null) {
 			Instance = new Maze();
@@ -13,11 +17,83 @@ public class Maze {
 	}
 	
 	private Maze() {
-		
+		// XXX Set mazeArray = to maze enum.value.
+		droneCurrentPosition = findCharInMazeArray('D');
+		goalPosition = findCharInMazeArray('G');
+		facedDirection = Direction.YPOS;
 	}
 	
+	public Coordinate getDroneCurrentCoordinates() {
+		return droneCurrentPosition;
+	}
 	
+	public Direction getDroneDirection() {
+		return facedDirection;
+	}
 	
+	public Coordinate getGoalCoordinates() {
+		return goalPosition;
+	}
 	
-
+	public boolean isObjectDetected() {
+		Coordinate infrontSpot = getCoordinateInfrontOfDrone();
+		char infrontChar = MazeArray[infrontSpot.getX()][infrontSpot.getY()];
+		return (infrontChar == 'X' || infrontChar == '|' || infrontChar == '_');
+	}
+	
+	public void moveFoward(){
+		if (isObjectDetected()) {
+			System.err.println("The drone ran full speed into an impassible object and exploded! Failed solving the maze!");
+			System.exit(0);
+		}
+		
+		Coordinate infront = getCoordinateInfrontOfDrone();
+		MazeArray[infront.getX()][infront.getY()] = 'D';
+		MazeArray[droneCurrentPosition.getX()][droneCurrentPosition.getY()] = ' ';
+		droneCurrentPosition = infront;
+	}
+	
+	public void turn(Direction directionToTurn) {
+		facedDirection = directionToTurn;
+	}
+	
+	private Coordinate findCharInMazeArray(char findMe) {
+		for(int i = 0; i < MazeArray.length; i++) {
+			for(int j = 0; j < MazeArray[i].length; j++) {
+				char value = MazeArray[i][j];
+				
+				if(value == findMe) {
+					return new Coordinate(i, j);
+				}
+			}
+		}
+		
+		System.err.println("Unable to find char: " + findMe + ", in MazeArray");
+		System.exit(0);
+		return null;
+	}
+	
+	private Coordinate getCoordinateInfrontOfDrone() {
+		int currentX = droneCurrentPosition.getX();
+		int currentY = droneCurrentPosition.getY();
+		int infrontX = currentX;
+		int infrontY = currentY;
+		
+		switch(facedDirection) {
+			case XPOS:
+				infrontX++;
+				break;
+			case XNEG:
+				infrontX--;
+				break;
+			case YPOS:infrontY++;
+				break;
+			case YNEG:
+				infrontY--;
+				break;
+		}
+		
+		return new Coordinate(infrontX, infrontY);
+	}
+	
 }
